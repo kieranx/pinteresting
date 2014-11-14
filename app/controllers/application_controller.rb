4e6e7d8e762ab474890e42fee688975715717425
kieranx <k@kierangill.me>
2014-11-14 18:23:44 +0000

    added names, password comfirmation changes and polished it up a bit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,3 +3,21 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 end
+
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end
+end
+
+def initials_for(user)
+  parts = user.split
+  sname = parts.first
+  sname += " #{parts.last[0]}." if parts.length > 1
+  sname
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :pins
 
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => 'registrations'}
   root 'pins#index'
   get "about" => "pages#about"
 
